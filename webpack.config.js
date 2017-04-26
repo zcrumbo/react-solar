86a88769577df9e563ca4aba034c4261119f0d9a
Zachary Crumbo <zcrumbo@mac.com>
2017-04-26 16:57:05 -0700

    upated scaffolding

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const dotenv = require('dotenv');
+const webpack = require('webpack');
+const HTMLPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const production = process.env.NODE_ENV === 'production';
+
+dotenv.load();
+
+module.exports = {
+  devtool: 'eval',
+  entry: `${__dirname}/app/entry.js`,
+  output: {
+    filename: 'bundle.js',
+    path: `${__dirname}/build`
+  },
+  plugins: [
+    new HTMLPlugin({
+      template: `${__dirname}/app/index.html`
+    }),
+    new ExtractTextPlugin('bundle.css'),
+    new webpack.DefinePlugin({
+      __API_URL__: JSON.stringify(process.env.API_URL),
+      __DEBUG__: JSON.stringify(!production),
+      __defaultUserPhoto__: JSON.stringify('/assets/user-anon.jpg')
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader'
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg|ico)$/i,
+        loader: 'file-loader?hash=sha512&digest=hex&name=[hash].[ext]'
+
+      },
+    ]
+  }
+};
