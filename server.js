f9cbfeb73846ccbbddd63fc400c13ebe1345c6f9
Zachary Crumbo <zcrumbo@mac.com>
2017-05-29 17:14:02 -0700

    added dotenv config

--- server.js
+++ server.js
@@ -6,10 +6,15 @@ const cors = require('cors');
 const Router = express.Router;
 const jsonParser = require('body-parser').json();
 const rp = require('request-promise');
+const dotenv = require('dotenv');
 const app = express();
 const PORT = process.env.PORT || 8080;
+const EGAUGE_ID = process.env.EGAUGE_ID;
 
+console.log('------------------------------------------>', EGAUGE_ID)
 const proxyRouter = Router();
+
+dotenv.load();
 
 app.use(express.static(`${__dirname}/build`));
 app.use(cors());
@@ -25,7 +30,7 @@ proxyRouter.post('/api/proxy/saved', jsonParser, (req, res, next)=>{
   const end = req.body.end || moment().unix();
   const interval = req.body.interval || 'm';
   const skip = req.body.skip || 9;
-  const url = `http://egauge16844.egaug.es/57A4C/cgi-bin/egauge-show?&t=${start}&f=${end}&s=${skip}&${interval}`;
+  const url = `http://egauge${EGAUGE_ID}.egaug.es/cgi-bin/egauge-show?&t=${start}&f=${end}&s=${skip}&${interval}`;
 
   rp(url)
   .then(pRes => {
@@ -35,9 +40,9 @@ proxyRouter.post('/api/proxy/saved', jsonParser, (req, res, next)=>{
 });
 
 proxyRouter.post('/api/proxy/instant', (req, res, next) => {
-  rp('http://egauge16844.egaug.es/57A4C/cgi-bin/egauge?inst')
+  rp(`http://egauge${EGAUGE_ID}.egaug.es/cgi-bin/egauge?inst`)
   .then( pRes => {
-    //console.log(pRes)
+   // console.log(url)
     res.send(pRes);
   })
   .catch(next);
