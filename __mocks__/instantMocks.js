637ebe5847b1c02748e87548e20fc900ca13d2ac
Zachary Crumbo <zcrumbo@mac.com>
2017-06-03 17:25:55 -0700

    tests

--- __mocks__/instantMocks.js
+++ __mocks__/instantMocks.js
@@ -1,11 +1,16 @@
 'use strict';
+import {Parser} from 'xml2js';
+import mock from './mockInstantResponse.xml';
 
-const fakeData = '<data serial="0x2d195193">
- <ts>1496451288</ts>
- <r t="P" n="Grid"><v>3412303433</v><i>-2257</i></r>
- <r t="P" n="Solar"><v>59137528234</v><i>2509</i></r>
- <r t="P" n="Solar+"><v>59329584119</v><i>2509</i></r>
- <r t="P" n="Heat Pump|"><v>15050181623</v><i>28</i></r>
- <r t="P" n="Water Heater"><v>2154113793</v><i>2</i></r>
-</data>
-'
+const parser = new Parser({mergeAttrs: true, charkey: 'val', async:true});
+
+export function fetchDataInstantProxy(){
+  return new Promise((resolve, reject)=> {
+    process.nextTick(() => {
+      (parser.parseString(mock, (err, results)=> {
+        if (err) reject(err);
+        resolve(results);
+      }));
+    });
+  });
+}
